8b1503c84788f238ef08f67b65ae6e46f3f9a17b
jared beckler and nic netzel <jared.beckler@gmail.com>
2016-02-24 15:46:12 -0800

    add input and display for new restaurants

--- app/app.php
+++ app/app.php
@@ -18,11 +18,31 @@
         return $app['twig']->render('index.html.twig', array('cuisines'=> Cuisine::getAll()));
     });
 
+    $app->get("/restaurants", function() use ($app) {
+        return $app['twig']->render('cuisines.html.twig', array('resturants' => Resturant::getAll()));
+    });
+
     $app->post("/cuisines", function() use ($app)
     {
         $new_cuisine = new Cuisine($_POST['new_cuisine']);
         $new_cuisine->save();
-      return $app['twig']->render('index.html.twig', array('cuisines'=>Cuisine::getAll()));
+      return $app['twig']->render('index.html.twig', array('cuisine'=>Cuisine::getAll()));
+    });
+
+    $app->get("/cuisines/{id}", function($id) use ($app){
+        $cuisine = Cuisine::find($id);
+      return $app['twig']->render('cuisines.html.twig', array('cuisine'=> $cuisine, 'restaurants' => $cuisine->getRestaurants()));
+    });
+
+    $app->post("/restaurants", function() use ($app)
+    {
+        $new_restaurant_name = $_POST['new_restaurant'];
+        $new_restaurant_description = $_POST['new_description'];
+        $cuisine_id = $_POST['cuisine_id'];
+        $new_restaurant = new Restaurant($new_restaurant_name, $new_restaurant_description, $cuisine_id, $id = null);
+        $new_restaurant->save();
+        $cuisine = Cuisine::find($cuisine_id);
+      return $app['twig']->render('cuisines.html.twig', array('cuisine' => $cuisine, 'restaurants' => $cuisine->getRestaurants()));
     });
 
     return $app;
